0864ae0320e540f52304b48f8b9553af041170a9
Bruce Chen <bruce30262@gmail.com>
2016-08-10 06:18:26 -0700

    add arm-toolchain setting

---
+++ set_arm_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# use qemu-static-user to emulate arm enviroment
+# will install gcc & g++ toolchain, libc & libstdc++
+# download armtool.alias from https://github.com/bruce30262/CTF
+# works on Ubuntu 16.04.1
+
+cur_dir=$(dirname $(readlink -f $BASH_SOURCE))
+
+# qemu-arm-static
+sudo apt-get install -y qemu-user-static &&\
+# arm (gcc & libc)
+sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross &&\
+# aarch64 (gcc & libc)
+sudo apt-get install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross &&\
+# arm (g++ & libstdc++)
+sudo apt-get install -y g++-arm-linux-gnueabihf libstdc++-5-dev-armhf-cross &&\
+# aarch64 (g++ & libstdc++)
+sudo apt-get install -y g++-aarch64-linux-gnu libstdc++-5-dev-arm64-cross &&\
+
+echo "setting alias..."
+curl -fsSL https://raw.githubusercontent.com/bruce30262/CTF/master/script/armtool.alias -o $cur_dir/aliases/arm-toolchain.alias
+echo "source ~/.zshrc to apply the latest arm-toolchain setting"
