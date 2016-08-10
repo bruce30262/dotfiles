f546f731434d4bb6227953335eb8cf69b128a681
Bruce Chen <bruce30262@gmail.com>
2016-08-10 19:14:52 +0800

    add more package in install.sh, add .gitignore in aliases/

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@
 
 cur_dir=$(dirname $(readlink -f $BASH_SOURCE))
 
+# update package info
+sudo apt-get -y update
+
 # install vim
 echo "installing vim..."
 sudo apt-get install -y vim
@@ -14,6 +17,17 @@ echo "done setting vim."
 # install curl
 echo "installing curl..."
 sudo apt-get install -y curl
+
+# install nasm, gdb, gcc & g++ family
+sudo apt-get install -y nasm
+sudo apt-get install -y gcc g++
+sudo apt-get install -y gcc-multilib g++-multilib
+sudo apt-get install -y gdb gdb-multiarch
+
+# other tools
+sudo apt-get install -y openssh-server 
+sudo apt-get install -y python-pip python-pip3
+sudo apt-get install -y nmap
 
 # install ruby
 echo "installing ruby..."
