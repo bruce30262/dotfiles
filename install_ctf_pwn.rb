19071eefc17530aeda60d39af564c4fb36f363b5
Bruce Chen <bruce30262@gmail.com>
2016-08-14 21:42:00 +0800

    update install_ctf_pwn.rb, add Z3, change angr to virtualenv

--- install_ctf_pwn.rb
+++ install_ctf_pwn.rb
@@ -7,7 +7,7 @@ include Myutil
 if not is_dir_exist("#{Dir.home}/CTF")
     puts "Installing bruce30262 CTF-toolkit..."
     system("git clone https://github.com/bruce30262/CTF.git ~/CTF")
-elsif
+else
     puts "bruce30262 CTF-toolkit already installed"
 end
 
@@ -15,7 +15,7 @@ end
 res = system("python -c 'import pwn'")
 if res == false # start installing
     puts "Installing pwntools..."
-    system("sudo apt-get install -y python2.7 python2.7-dev python-pip libffi-dev libssl-del libssh-dev")
+    system("sudo apt-get install -y python2.7 python2.7-dev python-pip libffi-dev libssl-dev libssh-dev")
     system("sudo pip install --upgrade git+https://github.com/Gallopsled/pwntools.git")
 else # already install
     puts "pwntools already installed"
@@ -31,7 +31,7 @@ end
 if not is_dir_exist("#{Dir.home}/libc-database")
     puts "Downloading libc-database..."
     system("git clone https://github.com/niklasb/libc-database.git ~/libc-database")
-elsif
+else
     puts "libc-database already exist"
 end
 
@@ -48,11 +48,31 @@ if not is_this_installed("r2")
     system("cd ~/radare2/sys && sudo ./install.sh")
 end
 
-# angr
-res = system("python -c 'import angr'")
-if res == false
-    system("sudo apt-get install -y python-dev libffi-dev build-essential virtualenvwrapper")
-    system("sudo pip install --upgrade angr")
+# Z3
+res = system("python -c 'import z3'")
+if res == false # start installing
+    puts "Installing z3..."
+    system("git clone https://github.com/Z3Prover/z3.git ~/z3")
+    system("cd ~/z3 && python scripts/mk_make.py --python")
+    system("cd ~/z3/build && make && sudo make install")
 else
-    puts "angr already installed"
+    puts "z3 already installed"
 end
+
+# angr
+# first check virtualenv
+SOURCE_VIRTUALENV = "source $(locate virtualenvwrapper.sh)"
+res = system("bash -c '#{SOURCE_VIRTUALENV} 2>/dev/null'")
+if res == false # no virtualenv
+    puts "Installing angr dependencies..."
+    system("sudo apt-get install -y python-dev libffi-dev build-essential virtualenvwrapper")
+    system("sudo updatedb && cp `locate virtualenvwrapper.sh` #{$CUR_DIR}/aliases/virtualenvwrapper.sh.alias'")
+end
+# then check angr
+resp = `bash -c '#{SOURCE_VIRTUALENV} && lsvirtualenv'`
+if resp.include?"angr"
+    puts "angr already installed"
+else # not install angr virtualenv yet
+    system("bash -c '#{SOURCE_VIRTUALENV} && mkvirtualenv angr && pip install --upgrade angr && deactivate'")
+end
+
