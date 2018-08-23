886474b1d600daa86cfe02d5054c1d4008b4b3ee
Bruce Chen <bruce30262@gmail.com>
2018-08-23 13:52:57 +0800

    Update install_fonts.sh

--- fonts/install_fonts.sh
+++ fonts/install_fonts.sh
@@ -2,6 +2,8 @@
 
 # script to install YaHei-Consolas-Hybrid & Monaco fonts
 
+CUR_DIR="$(dirname "$(readlink -f "$0")")"
+
 set -e
 
 if [ ! $(command -v wget) ]; then
@@ -16,13 +18,13 @@ echo 'Truetype folder Creating...'
 echo 'path:/usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid'
 echo 'path:/usr/share/fonts/truetype/Monaco'
 
-mkdir -p /usr/share/fonts/truetype/YaHei\ Consolas\ 
+mkdir -p /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
 mkdir -p /usr/share/fonts/truetype/Monaco
 
 echo 'Copy fonts to truetype folder...'
 
-cp ~/dotfiles/fonts/YaHei\ Consolas\ Hybrid\ 1.12.ttf /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
-cp ~/dotfiles/fonts/Monaco.ttf /usr/share/fonts/truetype/Monaco
+cp $CUR_DIR/YaHei\ Consolas\ Hybrid\ 1.12.ttf /usr/share/fonts/truetype/YaHei\ Consolas\ Hybrid
+cp $CUR_DIR/Monaco.ttf /usr/share/fonts/truetype/Monaco
 
 echo 'Modifying font permissions...'
 
