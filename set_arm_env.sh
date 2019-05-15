b97e0c60c32c7a915c94ce8628f5a7c94379a75c
Bruce Chen <bruce30262@pm.me>
2019-05-15 12:50:01 +0800

    Update set_arm_env.sh

--- set_arm_env.sh
+++ set_arm_env.sh
@@ -10,11 +10,11 @@ gxx_v=$(g++ -v  2>&1 | tail -1 | awk '{print $3}' | awk -F. '{print $1}')
 # qemu-arm-static
 sudo apt-get install -y qemu-user-static &&\
 # arm (gcc & libc)
-sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross &&\
+sudo apt-get install -y gcc-arm-linux-gnueabihf libc6-dev-armhf-cross gcc-arm-linux-gnueabi libc6-dev-armel-cross &&\
 # aarch64 (gcc & libc)
 sudo apt-get install -y gcc-aarch64-linux-gnu libc6-dev-arm64-cross &&\
 # arm (g++ & libstdc++)
-sudo apt-get install -y g++-arm-linux-gnueabihf libstdc++-$gxx_v-dev-armhf-cross &&\
+sudo apt-get install -y g++-arm-linux-gnueabihf libstdc++-$gxx_v-dev-armhf-cross g++-arm-linux-gnueabi libstdc++-$gxx_v-dev-armel-cross &&\
 # aarch64 (g++ & libstdc++)
 sudo apt-get install -y g++-aarch64-linux-gnu libstdc++-$gxx_v-dev-arm64-cross &&\
 
