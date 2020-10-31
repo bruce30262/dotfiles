76e9a870fa542cf79e685df92c46ea18f12f14a3
Bruce Chen <bruce30262@pm.me>
2020-10-31 16:21:33 +0800

    Update set_vim.sh

--- set_vim.sh
+++ set_vim.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Install vim and make sure it has python3 support
+sudo apt-get install -y vim vim-nox
 # Install powerline and font
 sudo apt-get install -y powerline fonts-powerline
 # Install my vim settings
