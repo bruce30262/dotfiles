b31ef1a5b255c8585841d9849324a7ea7a19bc84
Bruce Chen <bruce30262@pm.me>
2023-01-20 22:04:58 +0800

    Move set_nvim.sh to scripts/

--- scripts/set_nvim.sh
+++ scripts/set_nvim.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+# Can be run as a single script to install neovim settings
+
 set -ex
 
 sudo apt update
@@ -13,5 +16,5 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install vim settings
-stow -t ~/.config .config
+cd ~/dotfiles/.config && stow -t ~/.config/nvim/ nvim
 nvim -es -u ~/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
