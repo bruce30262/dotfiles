a2a20c4a65f86abe2943f82bb921c3d0122a4d44
Bruce Chen <bruce30262@pm.me>
2023-01-21 23:16:58 +0800

    Fix setup_zsh.sh

--- setup_scripts/setup_zsh.sh
+++ setup_scripts/setup_zsh.sh
@@ -10,8 +10,8 @@ sudo apt update
 sudo apt install -y zsh curl
 
 # Install zimfw
-curl -fLo ~/.config/zsh/.zim/zimfw.sh --create-dirs \
+curl -fLo ~/.config/zsh/.zim/zimfw.zsh --create-dirs \
   https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-chmod +x ~/.config/zsh/.zim/zimfw.sh
-zsh -c "source ~/.config/zsh/.zim/zimfw.sh install"
+chmod +x ~/.config/zsh/.zim/zimfw.zsh
+zsh -c "source ~/.config/zsh/.zim/zimfw.zsh install"
 
