04d863ca6964ac408696c55097b7bfe0353c3dc5
Bruce Chen <bruce30262@pm.me>
2023-01-21 23:02:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
 #!/bin/bash 
 
-# this script will install some tools that I found necessary
+# Install tools that I found necessary
+# Stow dotfiles
+# Setup nvim, zsh & tmux
 
 set -ex
-
-cur_dir=$(dirname $(readlink -f $BASH_SOURCE))
 
 # update package info
 sudo apt-get -y update
 
-# install & set vim
-echo "installing vim..."
-sudo apt-get install -y vim
-echo "setting vim..."
-bash $cur_dir/set_vim.sh
-echo "done setting vim."
+# install necessary tools
+sudo apt-get install -y stow \
+    gcc gdb \
+    python3 python-is-python3 python3-pip \
+    openssh-server \
+    netcat net-tools \
+    htop \
+    tig \
+    curl
 
-# install gdb, gcc
-sudo apt-get install -y gcc gdb
+# stow dotfiles
+stow -t ~ --no-folding .
 
-# other tools
-sudo apt-get install -y python3 python-is-python3
-sudo apt-get install -y htop
-sudo apt-get install -y ruby
-sudo apt-get install -y tig
-sudo apt-get install -y openssh-server 
-sudo apt-get install -y python3-pip
-sudo apt-get install -y netcat
-sudo apt-get install -y net-tools
-sudo apt-get install -y curl
+# setup env
+pushd setup_scripts
+./setup_nvim.sh
+./setup_zsh.sh
+./setup_tmux.sh
+popd
 
-echo "All done!"
-echo "Run ./install.rb -h for further configuration"
+set +x
+echo "All done !"
+
