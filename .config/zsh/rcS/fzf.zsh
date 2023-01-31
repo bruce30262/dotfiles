9b49f6e2c38745db1d94430cc2448f44a4417d6a
Bruce Chen <bruce30262@pm.me>
2023-01-31 22:07:34 +0800

    Add fzf.zsh

---
+++ .config/zsh/rcS/fzf.zsh
@@ -0,0 +1,14 @@
+FZF_RC_DIR=/usr/share/doc/fzf/examples/
+
+if [ -x "$(command -v fzf)"  ]
+then
+    # If we're in here, we assume fzf, ripgrep & fd-find are all installed
+    # https://github.com/junegunn/fzf/issues/634#issuecomment-1008200731
+    export FZF_DEFAULT_COMMAND="rg --files --follow --no-ignore-vcs --hidden -g '!{**/.git/*}'"
+    source $FZF_RC_DIR/key-bindings.zsh
+    source $FZF_RC_DIR/completion.zsh
+    # https://www.youtube.com/watch?v=aLMepxvUj4s
+    alias pf="fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down"
+else
+	echo "Install fzf first."
+fi
