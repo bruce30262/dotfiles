48854488b1738e0e1d9578679d563171c34097a4
Bruce Chen <bruce30262@pm.me>
2024-08-04 17:25:22 +0800

    Update ssh key helper scripts and switch to ed25519

--- helper_scripts/gen_sshkey.sh
+++ helper_scripts/gen_sshkey.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Simple wrapper script for generating rsa 4096 ssh key
+# Simple wrapper script for generating ed25519 ssh key
 
-ssh-keygen -t rsa -b 4096 "$@"
+ssh-keygen -t ed25519 "$@"
--- helper_scripts/set_git_sshkey.sh
+++ helper_scripts/set_git_sshkey.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+# Usage: EMAIL="<email>" KEY_FILE="<key_file>" ./set_git_sshkey.sh
+# If EMAIL or KEY_FILE isn't set in command line, it will just use the default one
+# The encryption is ed25519
+
 set -ex
 
 CUR_DIR=$(dirname "$0")
 
-DEFAULT_EMAIL="bruce30262@pm.me"
-DEFAULT_KEY_FILE=~/.ssh/ida_rsa
+EMAIL="${EMAIL:=bruce30262@pm.me}"
+KEY_FILE=${KEY_FILE:=~/.ssh/id_ed25519}
 
-$CUR_DIR/gen_sshkey.sh -C $DEFAULT_EMAIL -f $DEFAULT_KEY_FILE
-ssh-add $DEFAULT_KEY_FILE
+$CUR_DIR/gen_sshkey.sh -C $EMAIL -f $KEY_FILE
+eval "$(ssh-agent -s)"
+ssh-add $KEY_FILE
 
