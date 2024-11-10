31e87326d9e90bf9ae749cafd2e8524c65cb2fe7
Bruce Chen <bruce30262@pm.me>
2024-11-10 15:07:52 +0800

    Remove FZF_DEFAULT_COMMAND as the default one is good enough

--- .config/zsh/rcS/fzf.zsh
+++ .config/zsh/rcS/fzf.zsh
@@ -3,8 +3,6 @@ FZF_RC_DIR=$HOME/.config/fzf
 if [ -x "$(command -v fzf)"  ]
 then
     # If we're in here, we assume fzf, ripgrep & fd-find are all installed
-    # https://github.com/junegunn/fzf/issues/634#issuecomment-1008200731
-    export FZF_DEFAULT_COMMAND="rg --files --follow --no-ignore-vcs --hidden -g '!{**/.git/*}'"
     source $FZF_RC_DIR/key-bindings.zsh
     source $FZF_RC_DIR/completion.zsh
     # https://www.youtube.com/watch?v=aLMepxvUj4s
